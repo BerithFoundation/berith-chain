a382344894913a1b83910237a4c29ab564e477b4
CJS <bbattery@ibizsoftware.net>
2019-04-15 14:37:59 +0900

    add build.sh
    add build.sh

--- build.sh
+++ build.sh
@@ -1,30 +1,3 @@
 #!/bin/sh
 
-set -e
-
-if [ ! -f "build.sh" ]; then
-    echo "$0 must be run from the root of the repository."
-    exit 2
-fi
-
-# Create fake Go workspace if it doesn't exist yet.
-workspace="$PWD/build/_workspace"
-root="$PWD"
-berdir="$workspace/src/github.com/ibizsoftware/berith-chain"
-if [ ! -L "$berdir/berith" ]; then
-    mkdir -p "$berdir"
-    cd "$berdir"
-    ln -s ../../../../../. go-ethereum
-    cd "$root"
-fi
-
-# Set up the environment to use the workspace.
-GOPATH="$workspace"
-export GOPATH
-
-# Run the command inside the workspace.
-cd "$berdir/berith"
-PWD="$berdir/berith"
-
-# Launch the arguments with the configured environment.
-exec "$@"
+go build ./cmd/berith/
