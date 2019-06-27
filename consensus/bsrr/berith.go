10eb1c0bc485d5e05ce9a14f3a76cd425310e55f
서원국 <kukugi@ibizsoftware.net>
2019-06-27 10:20:01 +0900

    log with color

--- consensus/bsrr/berith.go
+++ consensus/bsrr/berith.go
@@ -35,6 +35,8 @@ import (
 	"github.com/BerithFoundation/berith-chain/log"
 	"github.com/BerithFoundation/berith-chain/params"
 	"github.com/BerithFoundation/berith-chain/rlp"
+	"github.com/gookit/color"
+
 	lru "github.com/hashicorp/golang-lru"
 )
 
@@ -44,7 +46,7 @@ const (
 	inmemorySignatures = 4096    // Number of recent block signatures to keep in memory
 
 	//stakingInterval = 10
-	wiggleTime      = 500 * time.Millisecond // Random delay (per signer) to allow concurrent signers
+	wiggleTime = 500 * time.Millisecond // Random delay (per signer) to allow concurrent signers
 )
 
 var (
@@ -492,13 +494,16 @@ func (c *BSRR) Finalize(chain consensus.ChainReader, header *types.Header, state
 		return nil, err
 	}
 
-	stakingList.Print()
-	fmt.Println("##############[FINALIZE]##############")
-	fmt.Println("NUMBER : ", header.Number.String())
-	fmt.Println("HASH : ", header.Hash().Hex())
-	fmt.Println("COINBASE : ", header.Coinbase.Hex())
-	fmt.Println("DIFFICULTY : ", header.Difficulty.String())
-	fmt.Println("######################################")
+	font := color.Yellow
+	if bytes.Compare(header.Coinbase.Bytes(), c.signer.Bytes()) == 0 {
+		font = color.Green
+	}
+	font.Println("##############[FINALIZE]##############")
+	font.Println("NUMBER : ", header.Number.String())
+	font.Println("HASH : ", header.Hash().Hex())
+	font.Println("COINBASE : ", header.Coinbase.Hex())
+	font.Println("DIFFICULTY : ", header.Difficulty.String())
+	font.Println("######################################")
 
 	if header.Coinbase != common.HexToAddress("0") {
 		//Epoch 이후에 처리
@@ -616,17 +621,11 @@ func (c *BSRR) CalcDifficulty(chain consensus.ChainReader, time uint64, parent *
 }
 func (c *BSRR) calcDifficulty(signer common.Address, chain consensus.ChainReader, time uint64, parent *types.Header) *big.Int {
 
-
-
 	target := parent
 	targetNumber := new(big.Int).Sub(parent.Number, big.NewInt(int64(c.config.Epoch)))
 	for target.Number.Cmp(big.NewInt(0)) > 0 && target.Number.Cmp(targetNumber) > 0 {
 		target = chain.GetHeader(target.ParentHash, target.Number.Uint64()-1)
 	}
-	fmt.Println("==================[DIFFICULTY]=================")
-	fmt.Println("HASH : ", target.Hash().Hex())
-	fmt.Println("NUMBER : ", target.Number.String())
-	fmt.Println("COMPARE : ", target.Number.Cmp(big.NewInt(0)))
 	if target.Number.Cmp(big.NewInt(0)) <= 0 {
 		return big.NewInt(1234)
 	}
