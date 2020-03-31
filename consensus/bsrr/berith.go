1e608f48917c6270df1621cf1fbf54aeff8ca006
kyumin2373 <kyumin2373@gmail.com>
2020-03-31 16:19:14 +0900

    solve problem A in Dev Comments

--- consensus/bsrr/berith.go
+++ consensus/bsrr/berith.go
@@ -395,16 +395,7 @@ func (c *BSRR) verifyCascadingFields(chain consensus.ChainReader, header *types.
 	if parent.Time.Uint64()+c.config.Period > header.Time.Uint64() {
 		return ErrInvalidTimestamp
 	}
-	// TODO : Check environments that have different time server
-	delayed, err := c.getDelay(int(header.Nonce.Uint64()))
-	if err != nil {
-		return ErrInvalidTimestamp
-	}
 
-	if parent.Time.Int64()+int64(c.config.Period)+int64(delayed.Seconds()) > time.Now().Unix() {
-		log.Warn("found invalid timestamp header", "number", header.Number.Uint64(), "hash", header.Hash().Hex(), "rank", header.Nonce.Uint64())
-		//return ErrInvalidTimestamp
-	}
 	// All basic checks passed, verify the seal and return
 	return c.verifySeal(chain, header, parents)
 }
