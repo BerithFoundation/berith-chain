f67c08a4d12d5d7286ae30366fe8ac51b351f04d
kyumin2373 <61676713+kyumin2373@users.noreply.github.com>
2020-04-27 10:02:00 +0900

    refactor: refactor warnings detected in go vet (#86)
    To improve code quality, refactor warnings detected in go vet
    
    Closes: #84
    
    Co-authored-by: kyumin <kumin2373@gmail.com>

--- params/config.go
+++ params/config.go
@@ -155,7 +155,7 @@ func (c *ChainConfig) String() string {
 	default:
 		engine = "unknown"
 	}
-	return fmt.Sprintf("{ChainID: %v Homestead: %v DAO: %v DAOSupport: %v EIP150: %v EIP155: %v EIP158: %v Byzantium: %v Constantinople: %v BIP1: %v Engine: %v}",
+	return fmt.Sprintf("{ChainID: %v Homestead: %v DAO: %v DAOSupport: %v EIP150: %v EIP155: %v EIP158: %v Byzantium: %v Constantinople: %v BIP1: %v BIP2: %v BIP3: %v Engine: %v}",
 		c.ChainID,
 		c.HomesteadBlock,
 		c.DAOForkBlock,
