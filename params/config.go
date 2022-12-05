6700eb05e660fd6f478b458ac14c65fea0de00cb
Igson <91243660+IGSON2@users.noreply.github.com>
2022-12-05 16:24:45 +0900

    feat: reduce minimum stake balance
    30000 BER -> 1 BER

--- params/config.go
+++ params/config.go
@@ -35,7 +35,7 @@ var (
 	TestnetGenesisHash = common.HexToHash("0x88484916701416d7f2990bed1d182c9e6001ed916e387669536f365451253cd0")
 )
 
-//[BERITH] Config Setting
+// [BERITH] Config Setting
 var (
 	// MainnetChainConfig is the chain parameters to run a node on the main network.
 	MainnetChainConfig = &ChainConfig{
